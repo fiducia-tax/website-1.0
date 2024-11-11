6e5de1efa7783db31cd0236bda5ceafc833e6674
fiducia-tax <admin@fiduciatax.com>
2024-11-11 14:48:38 -0500

    fixed top bar css updates

--- components/FixedTopBar.js
+++ components/FixedTopBar.js
@@ -1,9 +1,17 @@
 const FixedTopBar = () => {
     return (
         <div className="bg-primary">
-            <p className="text-background text-sm flex justify-center items-center py-2 px-9 max-lg:text-xs">
-                Working Hours: 6 AM to 9 PM US Eastern Time | Call: USA +1 (307) 207-1072 or IND +91 89788 52912 | Email: info@fiduciatax.com
-            </p>
+            <div className="text-background text-sm py-2 px-9 max-lg:text-xs lg:flex lg:justify-center lg:items-center lg:gap-9">
+                <p>
+                    Working Hours: 6 AM to 9 PM US Eastern Time
+                </p>
+                <p>
+                    Call: USA +1 (307) 207-1072 or IND +91 89788 52912
+                </p>
+                <p>
+                    Email: info@fiduciatax.com
+                </p>
+            </div>
         </div>
     )
 }
